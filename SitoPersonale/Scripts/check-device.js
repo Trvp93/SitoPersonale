6014218063bbca0c4666cd750c2d557dd711ad9e
Manuel <manuel.petroni93@hotmail.com>
2016-11-17 19:02:15 +0100

    bug-fixing #1 - 17/11/16 - 19:00

--- SitoPersonale/Scripts/check-device.js
+++ SitoPersonale/Scripts/check-device.js
@@ -1,53 +1,60 @@
-﻿alert("check-device.je - line 1");
+﻿//alert("check-device.je - line 1");
 var isMobile = {
     Android: function () {
-        return navigator.userAgent.match(/Android/i);
+        //return navigator.userAgent.match(/Android/i);
+        var result = navigator.userAgent.match(/Android/i) != null;
+        return result;
     },
     BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
+        var result = navigator.userAgent.match(/BlackBerry/i) != null;
+        return result;
     },
     iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        var result = navigator.userAgent.match(/iPhone|iPad|iPod/i) != null;
+        return result;
     },
     Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
+        var result = navigator.userAgent.match(/Opera Mini/i) != null;
+        return result;
     },
     Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
+        var result = navigator.userAgent.match(/IEMobile/i) != null;
+        return result;
     },
     any: function () {
         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
     }
 };
-console.log("isMobile.Android: " + isMobile.Android());
-console.log("sono in isMobile.BlackBarry: " + isMobile.BlackBerry());
-console.log("sono in isMobile.iOS: " + isMobile.iOS());
-console.log("sono in isMobile.Opera: " + isMobile.Opera());
-console.log("sono in isMobile.Windows: " + isMobile.Windows());
-console.log("sono in isMobile.any: " + isMobile.any());
-console.log(navigator.userAgent.toString());
-console.log(navigator.userAgent.match(/Mozilla/i));
+//console.log("isMobile.Android: " + isMobile.Android());
+//console.log("sono in isMobile.BlackBarry: " + isMobile.BlackBerry());
+//console.log("sono in isMobile.iOS: " + isMobile.iOS());
+//console.log("sono in isMobile.Opera: " + isMobile.Opera());
+//console.log("sono in isMobile.Windows: " + isMobile.Windows());
+//console.log("sono in isMobile.any: " + isMobile.any());
+//console.log(navigator.userAgent.toString());
+//console.log(navigator.userAgent.match(/Mozilla/i) != null);
 
-//if (isMobile.any()) {
-//    $('.carousel').carousel({
-//        interval: false
-//    });
-//}
+
+if (isMobile.any()) {
+    $('.carousel').carousel({
+        interval: false
+    });
+}
 
 
 //-------------- Nasconde i tasti whatsapp
 
-$('#wa-contact').hide();
-
+$("#wa-contact").hide();
+//$("div.modal-footer #wa-contact").hide();
 
 
 //------------- Verifica il SO del dispositivo attualmente connesso e mostra il tasto whatsapp adeguato
-//if (isMobile.Android) {
-//    $('#whatsappContact > i').show();
-//    $('#whatsappContact > i').attr("href", "intent://send/4912345678#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end");
-//}
+if (isMobile.Android()) {
+    $('#wa-contact').show();
+    $('#wa-contact').attr("href", "intent://send/+393899649683#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end");
+}
 
-//if (isMobile.iOS) {
-//    $('#whatsappContact > i').show();
-//    $('#whatsappContact > i').attr("href", "whatsapp://app?abid=+393899649683");
-//}
+if (isMobile.iOS()) {
+    $('#wa-contact').show();
+    $('#wa-contact').attr("href", "whatsapp://app?abid=+393899649683");
+}
