9eca7f5602b92ea8172520635ef25694285ae386
Manuel <manuel.petroni93@hotmail.com>
2016-11-18 14:55:31 +0100

    Fix bug whatsapp href nel menu - Migliorata la visualizzazzione mobile - Fix bug minori di visualizzazione - Pulizia codice -

--- SitoPersonale/Scripts/check-device.js
+++ SitoPersonale/Scripts/check-device.js
@@ -31,7 +31,7 @@ var Whatsapp = {
         $('#wa-contact').show();
         $("div.modal-footer #wa-contact").show();
 
-        //Imposta il collegamento a whatsapp - sintassi amdroid
+        //Imposta il collegamento a whatsapp - sintassi android
         $('#wa-contact').attr("href", "intent://send/+393899649683#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end");
         $("div.modal-footer #wa-contact").attr("href", "intent://send/+393899649683#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end");
     },
@@ -51,11 +51,27 @@ var Whatsapp = {
     }
 };
 
-// Blocca lo scorrimento dello slider su mobile
+function CallMobile() {
+    // Sostituisce l'icona di skype con quella del telefono
+    $('#icon-chiama').removeClass("fa-skype");
+    $('div.modal-footer #icon-chiama').removeClass("fa-skype");
+    $('#icon-chiama').addClass("fa-phone");
+    $('div.modal-footer #icon-chiama').addClass("fa-phone");
+
+    // Imposta l'href per abilitare le telefonate
+    $('#chiama').attr('href', 'tel:+393899649683');
+    $('div.modal-footer #chiama').attr('href', 'tel:+393899649683');
+}
+
+// Verifica se il dispositivo connesso è mobile e imposta il DOM di conseguenza
 if (isMobile.any()) {
+    // Blocca lo scorrimento dello slider su mobile
     $('.carousel').carousel({
         interval: false
     });
+
+    CallMobile();
+
 }
 
 
